f261906ce3431d854719635d2cfd78777682f334
hairunisa29 <hairunisa2909@outlook.com>
2024-01-06 16:20:48 +0700

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <p className="font-bold">tes</p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
