ad5adc5ac95934e90d28d22f7db9d14f21f89896
hairunisa29 <hairunisa2909@outlook.com>
2024-01-06 21:24:22 +0700

    setup font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["'Poppins'"],
+      },
+      colors: {
+        primary: "#072AC8"
+      }
+    },
   },
   plugins: [],
 }
