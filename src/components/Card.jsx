643c9cc556a42d9e8d051a0e9fce1dab79cdfeb8
hairunisa29 <hairunisa2909@outlook.com>
2024-01-07 15:57:03 +0700

    add transform on hover card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,6 @@
 function Card({ data }) {
   return (
-    <div
-      className="flex flex-col items-center justify-center rounded-lg p-4 shadow-md shadow-indigo-100 bg-white hover:bg-indigo-100 cursor-pointer"
-      // onClick={onClickProduct}
-    >
+    <div className="flex flex-col items-center justify-center rounded-lg p-4 shadow-md shadow-indigo-100 bg-white hover:bg-indigo-100 transform transition duration-1000 hover:scale-105">
       <img
         alt={data.name}
         src={data.sprites.other.dream_world.front_default}
