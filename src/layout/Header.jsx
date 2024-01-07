b90d7fb7796fe1719ab507836b3f30d745b1572e
hairunisa29 <hairunisa2909@outlook.com>
2024-01-08 04:31:52 +0700

    add navigation in header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,11 +3,16 @@ import PokemonLogo from "../assets/images/pokemon-logo.svg";
 
 function Header() {
   return (
-    <div className="sticky top-0 flex items-center justify-center md:justify-between p-8 md:p-4 bg-white">
+    <div className="sticky z-10 top-0 flex items-center justify-center md:justify-between p-8 md:p-4 bg-white">
       <Link to="/" className="flex items-center justify-center gap-4">
         <img src={PokemonLogo} alt="Logo" className="w-10" />
         <h1 className="font-bold text-3xl md:text-xl">Pokemon</h1>
       </Link>
+
+      <div className="hidden md:flex gap-4">
+        <Link to="/" className="font-bold">Home</Link>
+        <Link to="/my-collection" className="font-bold">My Collection</Link>
+      </div>
     </div>
   );
 }
