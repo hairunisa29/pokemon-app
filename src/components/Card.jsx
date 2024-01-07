9b67216423c85fbb95c219d1746c82201e2ac2d9
hairunisa29 <hairunisa2909@outlook.com>
2024-01-07 10:18:21 +0700

    create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+function Card({ data }) {
+  return (
+    <div
+      className="flex flex-col items-center justify-center rounded-lg p-4 shadow-md shadow-indigo-100 bg-white hover:bg-indigo-100 cursor-pointer"
+      // onClick={onClickProduct}
+    >
+      <img
+        alt={data.name}
+        src={data.sprites.other.dream_world.front_default}
+        className="w-32 h-32"
+      />
+
+      <h3 className="mt-2 font-semibold text-lg text-primary">{data.name}</h3>
+    </div>
+  );
+}
+
+export default Card;
