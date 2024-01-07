bf131c50dad96d78b341ca07ba244998e1134282
hairunisa29 <hairunisa2909@outlook.com>
2024-01-07 15:57:30 +0700

    set route detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import PokemonDetail from "./pages/PokemonDetail";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
       </Routes>
     </Layout>
   );
