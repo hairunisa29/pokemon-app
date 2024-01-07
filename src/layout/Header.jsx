b9f73264ce95ccdc65119f7cf21cd403f268f53e
hairunisa29 <hairunisa2909@outlook.com>
2024-01-08 00:49:16 +0700

    make header sticky

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ import PokemonLogo from "../assets/images/pokemon-logo.svg";
 
 function Header() {
   return (
-    <div className="flex items-center justify-center md:justify-between p-8 md:p-4">
+    <div className="sticky top-0 flex items-center justify-center md:justify-between p-8 md:p-4 bg-white">
       <Link to="/" className="flex items-center justify-center gap-4">
         <img src={PokemonLogo} alt="Logo" className="w-10" />
         <h1 className="font-bold text-3xl md:text-xl">Pokemon</h1>
