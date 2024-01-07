a8167d6054dd3e28c07bbf6d1ceadd38b56ec9f2
hairunisa29 <hairunisa2909@outlook.com>
2024-01-07 23:06:04 +0700

    add color secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         body: ["'Poppins'"],
       },
       colors: {
-        primary: "#072AC8"
-      }
+        primary: "#072AC8",
+        secondary: "#60D394",
+      },
     },
   },
   plugins: [],
-}
-
+};
